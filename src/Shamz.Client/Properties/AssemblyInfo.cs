5ef63ed6148b87d222fb57650a4d396663a6284a
Andy Sipe <ajs.general@gmail.com>
2013-09-05 07:49:44 -0400

    deleted Shamz.Server and renamed Shamz.Client to Shamz.Console

--- src/Shamz.Client/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Shamz.Client")]
-[assembly: AssemblyDescription("Shamz.Client")]
--- src/Shamz.Server/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Shamz.Server {
-  internal class Program {
-    private static void Main(string[] args) {}
-  }
-}
